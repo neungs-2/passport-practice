1be890ce80a8f323ee9e6434462c53876720ccd9
logan <gnlee95@gmail.com>
2022-02-06 01:49:20 +0900

    Update code using bcrypt and express-session

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ### **Install**
 
-- `$ npm i express ejs bcrypt`
+- `$ npm i express ejs bcrypt passport passport-local express-session express-flash`
 - `$ npm i --save-dev nodemon dotenv`
--- server.js
+++ server.js
@@ -1,11 +1,35 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+
+const initializePassport = require('./passport-config');
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
 
 const users = [];
 
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.render('index.ejs', { name: 'Logan' });
@@ -15,7 +39,14 @@ app.get('/login', (req, res) => {
   res.render('login.ejs');
 });
 
-app.post('/login', (req, res) => {});
+app.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true,
+  })
+);
 
 app.get('/register', (req, res) => {
   res.render('register.ejs');
@@ -36,4 +67,5 @@ app.post('/register', async (req, res) => {
   }
   console.log(users);
 });
+
 app.listen(3000);
